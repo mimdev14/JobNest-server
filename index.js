a57732cde3f26927b456f2c5cc08d67e4d679e53
MIM-0014 <fatemaaktermim1031@gmail.com>
2026-09-09 20:20:26 +0600

    upated indes.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const { connectDB } = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('JobNest API is running');
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+app.get("/", (req, res) => {
+  res.send("JobNest API is running");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-}); 
+connectDB().then(() => {
+  app.use("/api/auth", authRoutes);
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+});
